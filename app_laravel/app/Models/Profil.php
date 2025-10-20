636a27c713ed9fb57fb58fee99c4ffa81eaac938
nkoo0505 <dimus.id123@gmail.com>
2025-10-20 12:24:26 +0700

    KEGIATAN

--- app_laravel/app/Models/Profil.php
+++ app_laravel/app/Models/Profil.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Profil extends Model
 {
     use HasFactory;
+    protected $tables = 'ormawa';
+    protected $primaryKeys = 'ormawa_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'ormawa_id',
+        'judul',
+        'deskripsi',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'linkPendaftaran_url',
+        'poster',
+    ];
 }
